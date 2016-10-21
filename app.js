3346d462e859cacb532d027e9e013c2814dcd0e7
desundar <desundar@cisco.com>
2016-10-21 09:09:25 -0700

    Adding appid and secret

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
     // appId: process.env.MICROSOFT_APP_ID,
     // appPassword: process.env.MICROSOFT_APP_PASSWORD
-    appId: "Test",
-    appPassword: "Test"
+    appId: "98a5925b-2bd9-4fed-8991-3573dab9b93e",
+    appPassword: "3j5XeaokPAhCTWJ76HjYvY9"
     
 });
 var bot = new builder.UniversalBot(connector);
